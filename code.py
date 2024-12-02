fa47a68330d48d8d1fddae6805f868cd5461e66c
wanglankai <35916737+wanglankai@users.noreply.github.com>
2024-12-02 15:42:17 +0800

    Add files via upload

--- code.py
+++ code.py
@@ -1,261 +1,129 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import math
-
-import sys
-plt.rcParams["font.sans-serif"]=["SimHei"]
-plt.rcParams["axes.unicode_minus"]=False
+import numpy as np
+import random
 
 
-num = 10
-node = np.zeros((num, num), dtype=int)
+class RSUNode:
+    def __init__(self, node_id, storage_capacity, distance, violations, mileage):
+        self.node_id = node_id
+        self.storage_capacity = storage_capacity
+        self.distance = distance
+        self.violations = violations
+        self.mileage = mileage
+        self.remaining_storage = storage_capacity
+        self.incentive = 0
+        self.driving_record = self.calculate_driving_record(alpha=0.7, beta=0.3)
 
-np.fill_diagonal(node, 0)
+    def calculate_driving_record(self, alpha, beta):
+        return alpha * self.mileage - beta * self.violations
 
-for i in range(num):
-    for j in range(i + 1, num):
-        node[i, j] = np.random.randint(50, 100)
+    def calculate_competitiveness(self, weights):
+        competitiveness = (
+                weights["storage"] * self.remaining_storage +
+                weights["driving_record"] * self.driving_record -
+                weights["distance"] * self.distance
+        )
+        return competitiveness
 
-node = node + node.T - np.diag(node.diagonal())
-encourage = [0]*num
-AllC = [100] * num
-def findID(id):
-    AllC[id] = AllC[id] - 5
-    C = [0]*(num-1)
-    jj = 0
-    for i in range(len(AllC)):
-        if i!=id:
-            C[jj] = 1/AllC[i]
-            jj = jj + 1
-    # print(C)
-    RSUID = [0]*(num-1)
-    findRSUID = [0]*(int(num/2))
+    def receive_incentive(self, value):
+        self.incentive += value
 
-    distance = [0]*(num-1)
-    jj = 0
-    for i in range(len(node[id])):
-        if i!=id:
-            distance[jj] = node[id][i]
-            RSUID[jj] = i
-            jj = jj+1
-    d = np.sum(node, axis=1)
-    E = [distance,C]
+    def offload_data(self, data_size):
+        if data_size <= self.remaining_storage:
+            self.remaining_storage += data_size
+            self.incentive -= data_size
+            return True
+        return False
 
-    f = [[0] * len(distance),[0]* len(distance)]
-    for j in range(2):
-        for t in range(len(distance)):
-            f[j][t] = E[j][t] / np.sum(E[j])
-    k = 1 / (math.log(len(distance)))
-    H = [0] * 2
-    r = [0.0] * 2
-    for i in range(2):
-        sum = 0
-        for j in range(len(distance)):
-            sum = sum + f[i][j] * math.log(f[i][j])
-        H[i] = -1 * k * sum
-    for i in range(len(r)):
-        r[i] = (1 - H[i]) / (2 - np.sum(H))
-    # print(r)
-    w = [0.0] * len(distance)
-    for i in range(len(w)):
-        if max(distance) == min(distance):
-            distancei = 0
+    def __str__(self):
+        return (f"Node {self.node_id}: Storage={self.remaining_storage}/{self.storage_capacity}, "
+                f"Incentive={self.incentive:.2f}, DrivingRecord={self.driving_record:.2f}")
+
+
+def calculate_incentives(nodes, total_incentive_pool):
+    competitiveness_scores = np.array([node.calculate_competitiveness({
+        "storage": 0.4,
+        "driving_record": 0.3,
+        "distance": 0.3
+    }) for node in nodes])
+
+    normalized_scores = competitiveness_scores / competitiveness_scores.sum()
+    for i, node in enumerate(nodes):
+        node.receive_incentive(normalized_scores[i] * total_incentive_pool)
+
+
+def collaborative_storage(nodes, data_blocks):
+
+    sorted_nodes = sorted(nodes, key=lambda x: x.calculate_competitiveness({
+        "storage": 0.4,
+        "driving_record": 0.3,
+        "distance": 0.3
+    }), reverse=True)
+
+    for block in data_blocks:
+        for node in sorted_nodes:
+            if node.remaining_storage >= block:
+                node.remaining_storage -= block
+                print(f"Block of size {block} stored in Node {node.node_id}")
+                break
         else:
-            distancei = (distance[i] - min(distance)) / (max(distance) - min(distance))
-        if max(C) == min(C):
-            ci = 0
-        else:
-            ci = (C[i] - min(C)) / (max(C) - min(C))
+            print(f"Block of size {block} cannot be stored due to lack of capacity.")
 
-        w[i] = r[0]*distancei+r[1] * ci
-    w2 = sorted(w)
-    jj = len(w2)-1
-    while jj>=0:
-        if w2[jj] == 0:
-            w2[jj] = w2[jj+1]/2
-            for i in range(len(w)):
-                if w[i] == 0:
-                    w[i] = w2[jj+1]/2
-        jj = jj-1
-    jj = 0
-    for i in range(len(w2)):
-        for j in range(len(w)):
-            if (w[j] == w2[i]) & (jj<len(findRSUID)):
-                findRSUID[jj] = RSUID[j]
-                encourage[RSUID[j]] = encourage[RSUID[j]]+w[j]
-                jj = jj + 1
-    return findRSUID
 
-def findID2(id):
-    C = [0]*(num-1)
-    jj = 0
-    for i in range(len(AllC)):
-        if i!=id:
-            C[jj] = 1/AllC[i]
-            jj = jj + 1
-    RSUID = [0]*(num-1)
-    findRSUID = [0]*(int(num/2))
-
-    distance = [0]*(num-1)
-    jj = 0
-    for i in range(len(node[id])):
-        if i!=id:
-            distance[jj] = node[id][i]
-            RSUID[jj] = i
-            jj = jj+1
-    d = np.sum(node, axis=1)
-    E = [distance,C]
-
-    f = [[0] * len(distance),[0]* len(distance)]
-    for j in range(2):
-        for t in range(len(distance)):
-            f[j][t] = E[j][t] / np.sum(E[j])
-    k = 1 / (math.log(len(distance)))
-    H = [0] * 2
-    r = [0.0] * 2
-    for i in range(2):
-        sum = 0
-        for j in range(len(distance)):
-            sum = sum + f[i][j] * math.log(f[i][j])
-        H[i] = -1 * k * sum
-    for i in range(len(r)):
-        r[i] = (1 - H[i]) / (2 - np.sum(H))
-    w = [0.0] * len(distance)
-    for i in range(len(w)):
-        if max(distance) == min(distance):
-            distancei = 0
-        else:
-            distancei = (distance[i] - min(distance)) / (max(distance) - min(distance))
-        if max(C) == min(C):
-            ci = 0
-        else:
-            ci = (C[i] - min(C)) / (max(C) - min(C))
-
-        w[i] = r[0]*distancei+r[1] * ci
-    w2 = sorted(w)
-    jj = len(w2)-1
-    while jj>=0:
-        if w2[jj] == 0:
-            w2[jj] = w2[jj+1]/2
-            for i in range(len(w)):
-                if w[i] == 0:
-                    w[i] = w2[jj+1]/2
-        jj = jj-1
-    jj = 0
-    for i in range(len(w2)):
-        for j in range(len(w)):
-            if (w[j] == w2[i]) & (jj<len(findRSUID)):
-                findRSUID[jj] = RSUID[j]
-                encourage[RSUID[j]] = encourage[RSUID[j]]+w[j]
-                jj = jj + 1
-    return findRSUID
-
-def changeIDstorage(id,rr,changeid):
-    storageCopy = AllC.copy()
-    storageCopy[changeid] = storageCopy[changeid]+rr*100
-    C = [0]*(num-1)
-    jj = 0
-    for i in range(len(storageCopy)):
-        if i!=id:
-            C[jj] = 1/storageCopy[i]
-            jj = jj + 1
-    RSUID = [0]*(num-1)
-    findRSUID = [0]*(int(num/2))
-
-    distance = [0]*(num-1)
-    jj = 0
-    for i in range(len(node[id])):
-        if i!=id:
-            distance[jj] = node[id][i]
-            RSUID[jj] = i
-            jj = jj+1
-    d = np.sum(node, axis=1)
-    E = [distance,C]
-
-    f = [[0] * len(distance),[0]* len(distance)]
-    for j in range(2):
-        for t in range(len(distance)):
-            f[j][t] = E[j][t] / np.sum(E[j])
-    k = 1 / (math.log(len(distance)))
-    H = [0] * 2
-    r = [0.0] * 2
-    for i in range(2):
-        sum = 0
-        for j in range(len(distance)):
-            sum = sum + f[i][j] * math.log(f[i][j])
-        H[i] = -1 * k * sum
-    for i in range(len(r)):
-        r[i] = (1 - H[i]) / (2 - np.sum(H))
-    # print(r)
-    w = [0.0] * len(distance)
-    for i in range(len(w)):
-        if max(distance) == min(distance):
-            distancei = 0
-        else:
-            distancei = (distance[i] - min(distance)) / (max(distance) - min(distance))
-        if max(C) == min(C):
-            ci = 0
-        else:
-            ci = (C[i] - min(C)) / (max(C) - min(C))
-
-        w[i] = r[0]*distancei+r[1] * ci
-    w2 = sorted(w)
-    jj = len(w2)-1
-    while jj>=0:
-        if w2[jj] == 0:
-            w2[jj] = w2[jj+1]/2
-            for i in range(len(w)):
-                if w[i] == 0:
-                    w[i] = w2[jj+1]/2
-        jj = jj-1
-    jj = 0
-    for i in range(len(w2)):
-        for j in range(len(w)):
-            if (w[j] == w2[i]) & (jj<len(findRSUID)):
-                findRSUID[jj] = RSUID[j]
-                encourage[RSUID[j]] = encourage[RSUID[j]]+w[j]
-                jj = jj + 1
-    return findRSUID
-def delStorage():
-    timesid = [0]*num
-    for i in range(len(AllC)):
-        findRSUID = findID2(i)
-        for j in range(len(findRSUID)):
-            timesid[findRSUID[j]] = timesid[findRSUID[j]] + 1
-    for x in range(len(timesid)):
-        if AllC[x] <100:
-            if timesid[x]<=(0.3*num):
-                unloadRSUNum[x] = unloadRSUNum[x] + 1
-                rr = 0
-                times = 0
-                while times<=(0.8*num):
-                    rr = rr + 0.05
-                    times = 0
-                    for i in range(len(AllC)):
-                        findRSUID = changeIDstorage(i,rr,x)
-                        if x in findRSUID:
-                            times = times+1
-                if (AllC[x] + rr*100)<=100:
-                    AllC[x] = AllC[x] + rr*100
+def offload_data(nodes, lower_threshold, upper_threshold):
+    for node in nodes:
+        while True:
+            competitiveness = node.calculate_competitiveness({
+                "storage": 0.4,
+                "driving_record": 0.3,
+                "distance": 0.3
+            })
+            if competitiveness < lower_threshold:
+                data_to_offload = min(node.remaining_storage, 10)
+                if node.offload_data(data_to_offload):
+                    print(f"Node {node.node_id} offloaded {data_to_offload} units of data.")
                 else:
-                    AllC[x] = 100
-
-piecenum = [50,75,100,125,150,175,200]
-unloadNum = [[0]*num]*len(piecenum)
-
-for a in range(len(piecenum)):
-    unloadRSUNum = [0]*num
-    for b in range(piecenum[a]):
-
-        id = np.random.randint(num)
-        findRSUID = findID(id)
-        for i in range(len(findRSUID)):
-            AllC[findRSUID[i]] = AllC[findRSUID[i]] - 5
-        delStorage()
-    unloadNum[a] = unloadRSUNum
-    unloadNum = np.array(unloadNum)
-print(unloadNum.sum(axis=1))
+                    print(f"Node {node.node_id} cannot offload more data (insufficient storage).")
+                    break
+            elif competitiveness >= upper_threshold:
+                print(f"Node {node.node_id} reached the upper threshold. Stop offloading.")
+                break
+            else:
+                break
 
 
+if __name__ == "__main__":
+    random.seed(42)
+    nodes = [
+        RSUNode(
+            node_id=i,
+            storage_capacity=random.randint(80, 150),
+            distance=random.randint(5, 20),
+            violations=random.randint(0, 5),
+            mileage=random.randint(200, 1000)
+        )
+        for i in range(1, 21)
+    ]
 
+    print("Initial State:")
+    for node in nodes:
+        print(node)
 
+    total_incentive_pool = 500
+    calculate_incentives(nodes, total_incentive_pool)
+    print("\nAfter Incentive Allocation:")
+    for node in nodes:
+        print(node)
+
+    data_blocks = [30, 40, 50, 60, 70]
+    print("\nCollaborative Storage Process:")
+    collaborative_storage(nodes, data_blocks)
+
+    lower_threshold = 30
+    upper_threshold = 90
+    print("\nData Offloading Process:")
+    offload_data(nodes, lower_threshold, upper_threshold)
+
+    print("\nFinal State:")
+    for node in nodes:
+        print(node)
